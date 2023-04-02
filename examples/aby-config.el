67c52a2846d2052588adc2c870fe2a55a523a632
Ruben Philipp <me@rubenphilipp.com>
2023-04-03 00:31:14 +0200

    renamed exampels folder

---
+++ examples/aby-config.el
@@ -0,0 +1,30 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; FILE
+;;; aby-config.el
+;;;
+;;; DESCRIPTION
+;;; This file contains the configuration for the Aby package. I.e. mainly the
+;;; location of the fragments directory.
+;;; NB: It is necessary that the Aby package is available.
+;;;
+;;; AUTHOR
+;;; Ruben Philipp <me@rubenphilipp.com>
+;;;
+;;; CREATED
+;;; 2023-04-01
+;;;
+;;; $$ Last modified:  17:00:10 Sun Apr  2 2023 CEST
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; must end with a slash!!
+(setq aby-fragments-dir "/Users/rubenphilipp/code/aby/example/fragments/")
+
+(setq aby-auto-replacements
+      `((author "{{author}}" ,+rp-document-author+)
+        (author-email "{{author-email}}" ,(concat "<"
+                                   +rp-document-author-mail+
+                                   ">"))
+        (created "{{created}}" ,(format-time-string "%Y-%m-%d"))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; EOF aby-config.el
