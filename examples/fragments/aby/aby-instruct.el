8c05220d504dfb83bda17c3be23f8f1f8324ca38
Ruben Philipp <me@rubenphilipp.com>
2023-04-14 00:00:57 +0200

    improved aby-instruct snippet

--- examples/fragments/aby/aby-instruct.el
+++ examples/fragments/aby/aby-instruct.el
@@ -11,26 +11,19 @@
 ;;; CREATED
 ;;; {{created}}
 ;;;
-;;; $$ Last modified: 
+;;; $$ Last modified:  00:00:06 Fri Apr 14 2023 CEST
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(let* (;; the file containing the template data
-       ;; relative to this instruction-file
-       (fragment "{{fragment-file}}")
-       ;; a list with static-replacements (cf. doc)
-       (static-rep '())
-       ;; a list with ask-replacements
-       (ask-rep '(;; the first element is the symbol for the variable
-                  ;; the second element is the replacement-regex
-                  ;; the third element is the prompt (optional)
-                  ;; the fourth element is a documentation comment (optional)
+(let* ((fragment "{{fragment-file}}")
+       (static-rep '(
+                     ;; (var-n regex-n value-n comment-n)
+                     ))
+       (ask-rep '(
+                  ;; (var-n regex-n value-n comment-n)
                   ))
-       ;; a list with dynamic replacements
-       ;; the function must take the assoc list with
-       ;; the replacements as first argument (cf. doc)
-       (dynamic-rep '())
-  ;; this constructs the instruction association list
-  ;; and must be the return value of this file
+       (dynamic-rep '(
+                      ;; (var-n regex-n prompt-n comment-n)
+                      ))
   (aby-instruct fragment
                 static-rep
                 ask-rep
