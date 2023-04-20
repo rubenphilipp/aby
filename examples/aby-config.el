1544f60a5418c004b4302e74cd2090d960daeeda
Ruben Philipp <me@rubenphilipp.com>
2023-04-20 12:22:54 +0200

    fixed #3

--- examples/aby-config.el
+++ examples/aby-config.el
@@ -13,16 +13,25 @@
 ;;; CREATED
 ;;; 2023-04-01
 ;;;
-;;; $$ Last modified:  09:32:03 Mon Apr  3 2023 CEST
+;;; $$ Last modified:  12:22:26 Thu Apr 20 2023 CEST
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;; must end with a slash!!
 (setq aby-fragments-dir "/Users/mnemosyne/code/aby/example/fragments/")
 
+;;; These are replacements which will be performed
+;;; automatically in every fragment (both in instruction-file-based
+;;; fragments and plain-fragments)
 (setq aby-auto-replacements
       `((author "{{author}}" "Mnemosyne")
-        (author-email "{{author-email}}" "mnemosyne@nil.org"
+        (author-email "{{author-email}}" "mnemosyne@nil.org")
         (created "{{created}}" ,(format-time-string "%Y-%m-%d"))))
+
+;;; This is the default file extension for aby instruction-files
+(setq aby-file-extension "aby.el")
+
+;;; This is a RegEx indicating Aby plain-fragments
+(setq aby-plain-fragment-indicator ".abyss\\.?+\\..+$")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; EOF aby-config.el
